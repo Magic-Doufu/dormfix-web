2964ebb6158171d3fda7358c7b2cedc1a93d7a46
M.T.P.T <gtrx8fd3ds@gmail.com>
2018-03-09 18:39:26 +0800

    Revert "origin"
    This reverts commit dee8b6b6d35fb3207a16679c96b40a796f548968.

--- application/views/components/button_back.php
+++
@@ -1,8 +0,0 @@
-<div class="row col-md-8 col-md-offset-2 pd-0">
-    <button back class="btn btn-info btn-square btn-block">回上頁</button>
-</div>
-<script type="text/javascript">
-    $('button[back]').click(function(){
-        history.length > 1 ? history.back() : $(location).attr('href',"<?=base_url('index.php/request_list');?>");
-    })
-</script>
